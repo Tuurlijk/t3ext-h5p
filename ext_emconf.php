bed0fcc2601b4daea7f3ba37a00e3a0f24794f6b
Michiel Roos <michiel@michielroos.com>
2020-10-26 16:33:54 +0100

    Prepare for release 0.3.12

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF['h5p'] = [
     'dependencies'     => '',
     'state'            => 'stable',
     'uploadfolder'     => 0,
-    'version'          => '10.0.4',
+    'version'          => '0.3.12',
     'constraints'      => [
         'depends'      => [
-            'typo3' => '7.0.0-10.4.99',
+            'typo3' => '7.0.0-9.5.99',
         ], 'conflicts' => [],
         'suggests'     => [],
     ],
