6aa8ccab0af5e686f7bef85b1165550fe7d09716
Michiel Roos <michiel@michielroos.com>
2022-03-12 18:20:44 +0100

    Prepare release

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,23 @@
 <?php
 
 $EM_CONF['h5p'] = [
-    'title'            => 'H5p',
-    'description'      => 'Create and add rich content to your website for free. Some examples of what you get with H5P are Interactive Video, Quizzes, Collage and Timeline.',
-    'category'         => 'fe',
-    'author'           => 'Michiel Roos',
-    'author_company'   => 'Michiel Roos',
-    'author_email'     => 'michiel@michielroos.com',
-    'state'            => 'stable',
-    'version'          => '11.0.10',
-    'constraints'      => [
-        'depends'      => [
+    'title'          => 'H5p',
+    'description'    => 'Create and add rich content to your website for free. Some examples of what you get with H5P are Interactive Video, Quizzes, Collage and Timeline.',
+    'category'       => 'fe',
+    'author'         => 'Michiel Roos',
+    'author_company' => 'Michiel Roos',
+    'author_email'   => 'michiel@michielroos.com',
+    'state'          => 'stable',
+    'version'        => '11.5.8',
+    'constraints'    => [
+        'depends'   => [
             'typo3' => '11.5.0-11.5.99',
-        ], 'conflicts' => [],
-        'suggests'     => [],
+        ],
+        'conflicts' => [],
+        'suggests'  => [],
     ],
-    'autoload'         => [
-        'psr-4' => ['MichielRoos\\H5p\\' => 'Classes']
+    'autoload'       => [
+        'classmap' => ["Resources/Public/Lib"],
+        'psr-4'    => ['MichielRoos\\H5p\\' => 'Classes']
     ],
 ];
